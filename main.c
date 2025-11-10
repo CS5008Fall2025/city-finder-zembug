6832361d7a74ee7e01f5d2883b2bfa7a654a34ce
Emily D <embug37@gmail.com>
2025-11-09 20:03:37 -0500

    started splash message in main

---
+++ main.c
@@ -0,0 +1,18 @@
+void print_splash_message() {
+    printf("*****Welcome, traveler of the realm! Do you seek paths long forgotten?*****\n");
+    printf("You stand at the crossroads of legend, where cities whisper and roads remember.\n");
+    printf("Your quest: to seek the swiftest path through lands both near and far.\n");
+    printf("Commands:\n");
+    printf("\tlist - reveal the cities of the realm\n");
+    printf("\t<city1> <city2> - unveil the quickest path between two cities\n");
+    printf("\thelp - consult the scroll of guidance\n");
+    printf("\texit - abandon quest\n");
+    printf("*******************************************************\n");
+}
+
+int main() {
+    print_splash_message();
+    // Further implementation of the main function would go here.
+    return 0;
+}
+
