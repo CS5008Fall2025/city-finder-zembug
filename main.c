ebb1feab895c04da302486ac7b361f68d7a739db
Emily D <embug37@gmail.com>
2025-11-11 16:40:05 -0500

    updates to main.c

--- main.c
+++ main.c
@@ -21,17 +21,45 @@ void print_splash_message() {
 
 
 int main() {
+    print_splash_message();
+
     char input[137];
     char city1[69], city2[69];
 
-    if (isatty(fileno(stdin))) {
-        print_splash_message();
-    }
-
-    int num_cities;
-    char **city_names = load_cities("vertices.txt", &num_cities);
-
     while (1) {
+        printf("Where do you want to go today? ");
+        fgets(input, sizeof(input), stdin);
+
+        if (strcomp(input, "exit") == 0) {
+            printf("Farewell, traveler. May the trees guide your path and the wind speak your story.\n");
+            break;
+        } 
+        else if (strcmp(input, "help") == 0) {
+            print_scroll_of_guidance();
+        } 
+        else {
+            printf("Invalid Command\n");
+            print_scroll_of_guidance();
+        }
+    }
+    return 0;
+}
+    
+   
+    
+
+    //int num_cities;
+    //char **city_names = load_cities("vertices.txt", &num_cities);
+    //printf("Loaded %d cities.\n", num_cities);
+
+
+
+
+
+
+
+    /**
+   while (1) {
         
         printf("Where do you want to go today? ");
         
@@ -62,7 +90,7 @@ int main() {
     }
 
     return 0;
-}
+} */
 
 
 
